ef3a9693b103f4d438815379760ff6868955bc8f
jorgeorlandogonzalezguzman <jorge.o.gonzalez.dev@gmail.com>
2022-07-24 02:34:42 -0500

    initial

---
+++ source/Post.py
@@ -0,0 +1,9 @@
+import psycopg2
+import logging
+
+class Post:
+    def __init__(self, post_id):
+        self.post_id= post_id
+        self.post_title=""
+        self.post_image_id=""
+        self.post_content=""
